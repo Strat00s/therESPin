76dccd775d6bade175e1a856731f2541e841b6a3
Lukas <l.bastyr@seznam.cz>
2021-09-21 21:41:58 +0200

    nice wafeform generation

--- test/sine_gen.cpp
+++ test/sine_gen.cpp
@@ -2,54 +2,55 @@
 #include <stdio.h>
 #include <math.h>
 
-#define TABLE_SIZE 1024
+#define TABLE_SIZE 4096
 #define AMPLITUDE 100
 
 int main() {
     int sine[TABLE_SIZE] = {0};
     int square[TABLE_SIZE] = {0};
     int triangle[TABLE_SIZE] = {0};
+    float f_sine[TABLE_SIZE] = {0.0};
+    float f_square[TABLE_SIZE] = {0.0};
+    float f_triangle[TABLE_SIZE] = {0.0};
 
-    //generate sine
-    printf("Sine:\n");
+    printf("dT,category,format,val\n");
+    //printf("dT,val\n");
+    float delta = (float)AMPLITUDE / (TABLE_SIZE / 2.0);
+    int quater = TABLE_SIZE / 4;
     for (int i = 0; i < TABLE_SIZE; i++) {
-        sine[i] = (int)(AMPLITUDE * sin(2.0 * M_PI * (1.0 / TABLE_SIZE) * i));
-        printf("%d,", sine[i]);
-    }
-    //generate square
-    printf("\nSquare:\n");
-    for (int i = 0; i < TABLE_SIZE; i++) {
+        f_sine[i] = (AMPLITUDE * sin(2.0 * M_PI * (1.0 / TABLE_SIZE) * i));
+        sine[i] = (int)f_sine[i];
+
         if (i < TABLE_SIZE / 2) {
-            square[i] = AMPLITUDE;
+            f_square[i] = AMPLITUDE;
+            square[i] = (int)AMPLITUDE;
         }
         else {
-            square[i] = -AMPLITUDE;
+            f_square[i] = -AMPLITUDE;
+            square[i] = (int)-AMPLITUDE;
         }
-        printf("%d,", square[i]);
-    }
 
-    //generate triangle
-    printf("\nTirangle:\n");
-    float delta = (float)AMPLITUDE / (TABLE_SIZE / 2.0);
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        //if (i < TABLE_SIZE / 2) {
-        //    triangle[i] = (int)((-AMPLITUDE / 4) + delta * i);
-        //}
-        //else {
-        //    triangle[i] = (int)(((AMPLITUDE / 4) - delta * i) + AMPLITUDE);
-        //}
         if (i < TABLE_SIZE / 4) {
-            triangle[i] = delta * i;
+            f_triangle[i] = (delta * i * 2);
+            triangle[i] = (int)f_triangle[i];
         }
-        else if (i < TABLE_SIZE / 2) {
-            triangle[i] = (int)AMPLITUDE - delta * i;
+        else if (i < (TABLE_SIZE / 2)) {
+            f_triangle[i] = (AMPLITUDE * 2 - delta * i * 2);
+            triangle[i] = (int)f_triangle[i];
         }
         else if (i < (TABLE_SIZE / 4) * 3) {
-            triangle[i] = (int)(- delta * i);
+            f_triangle[i] = (AMPLITUDE * 2 - delta * i * 2);
+            triangle[i] = (int)f_triangle[i];
         }
         else {
-            triangle[i] = -AMPLITUDE + delta * i;
+            f_triangle[i] = (-AMPLITUDE * 4 + delta * i * 2);
+            triangle[i] = (int)f_triangle[i];
         }
-        printf("%d,", triangle[i]);
+        printf("%d,sine,red,%d\n", i, sine[i]);
+        printf("%d,square,green,%d\n", i, square[i]);
+        printf("%d,triangle,blue,%d\n", i, triangle[i]);
+        printf("%d,f_sine,violet,%f\n", i, f_sine[i]);
+        printf("%d,f_square,yellow,%f\n", i, f_square[i]);
+        printf("%d,f_triangle,pink,%f\n", i, f_triangle[i]);
     }
 }
